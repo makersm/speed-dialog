3774e954d0494e06bd67b8b0cbf3a521d1900c44
sayyo1120 <sayyo1120@gmail.com>
2019-06-12 23:50:10 +0900

    fix: url validation function

--- js/database.js
+++ js/database.js
@@ -160,9 +160,9 @@ $( function() {
         var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
             '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
             '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+            '(\\:\\d+)?(\\/[\\%-a-z\\d_.~+]*)*'+ // port and path
             '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-            '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+            '(\\#[\\/\\%-a-z\\d_]*)?$','i'); // fragment locator
         return !!pattern.test(str);
     }
 
